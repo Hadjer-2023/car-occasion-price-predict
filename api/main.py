bcd65f8712d31e396e68f355f599e4a9ee1a949b
Github2199 <150786148+Github2199@users.noreply.github.com>
2024-03-13 12:28:02 +0100

    maj fastapi

--- api/main.py
+++ api/main.py
@@ -15,6 +15,7 @@ model = joblib.load("model.joblib")
 templates = Jinja2Templates(directory="C:\\Users\\Utilisateur2\\Documents\\FORM-DEVIA\\4-260224-220324 REGRESSION-CLUSTERING\\5Projet\\car-occasion-price-predict\\web\\templates\\")
 
 class PredictionForm(BaseModel):
+    name: str  
     year: int
     kilometers_driven: int
     fuel_type: str
@@ -25,8 +26,9 @@ class PredictionForm(BaseModel):
     power: float
     seats: int
 
-# Example data for testing
+# exemple pour tester
 example_data = {
+    "name": "Maruti Alto K10 LXI CNG", 
     "year": 2014,
     "kilometers_driven": 40929,
     "fuel_type": "CNG",
@@ -42,22 +44,21 @@ example_data = {
 async def predict(form: PredictionForm = Form(...)):
     try:
         # Prepare data for prediction
-        features = [form.year, form.kilometers_driven, form.fuel_type, form.transmission, form.owner_type, form.mileage, form.engine, form.power, form.seats]
+        features = [form.name,form.year, form.kilometers_driven, form.fuel_type, form.transmission, form.owner_type, form.mileage, form.engine, form.power, form.seats]
+
+        # Make prediction
+        predicted_price = model.predict([features])[0]
+
 
         # Make prediction
         predicted_price = model.predict([features])[0]
 
         # Return the result to the prediction.html page
         return templates.TemplateResponse("prediction.html", {"predicted_price": predicted_price})
-    except HTTPException as e:
+    except Exception as e:
         # Print the error details to the console
         print(f"An error occurred: {e}")
-        
-        # Return validation details as JSON
-        return JSONResponse(content={"detail": e.errors()}, status_code=e.status_code)
-    except Exception as e:
-        # Handle other exceptions
-        print(f"An error occurred: {e}")
+        # Raise an HTTP exception to display a specific error page
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.get("/prediction", response_class=HTMLResponse)
